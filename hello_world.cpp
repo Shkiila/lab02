0967376b30d833ff2fd857e2d7e013722147566a
Shkila <serafinkosty@gmail.com>
2023-03-02 09:33:30 +0300

    add descriptions

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,6 +3,6 @@
 
 int main(){
     std::string name;
-    std::cin>>name;
-    std::cout<<"Hello world from "<<name<<std::endl;
+    std::cin>>name;    //читает имя из строки
+    std::cout<<"Hello world from "<<name<<std::endl;    //выводит сообщение в терминал с обращением по имени
 }
