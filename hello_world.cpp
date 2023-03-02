e68233fd532eb4c5443e57e972498daa3fc9b88c
Shkila <serafinkosty@gmail.com>
2023-03-02 09:49:11 +0300

    add clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <string>
 
+<<<<<<< HEAD
 int main(){
     std::string name;
     std::cin>>name;    //read name 
     std::cout<<"Hello world from "<<name<<std::endl;    //prints a message to the terminal addressing by name
 }
+=======
+int
+main()
+{
+  std::string name;
+  std::cin >> name; //читает имя из строки
+  std::cout << "Hello world from " << name
+            << std::endl; //выводит сообщение в терминал с обращением по имени
+}
+>>>>>>> 53a61b4 (add clang-format)
